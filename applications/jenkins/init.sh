89a633b242e1889496b06f30d4ee44aef48a1e59
sseraj007 <sseraj007@gmail.com>
2021-09-13 19:13:21 +0530

    Update init.sh

--- applications/jenkins/init.sh
+++ applications/jenkins/init.sh
@@ -10,9 +10,16 @@ set -o nounset
 echo "==> Starting deployment"
 
 # Installing .net SDK and Runtime
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb sudo dpkg -i packages-microsoft-prod.deb rm packages-microsoft-prod.deb
-sudo apt-get update; \ sudo apt-get install -y apt-transport-https && \ sudo apt-get update && \ sudo apt-get install -y dotnet-sdk-3.1
-sudo apt-get update; \ sudo apt-get install -y apt-transport-https && \ sudo apt-get update && \ sudo apt-get install -y aspnetcore-runtime-3.1
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb rm packages-microsoft-prod.deb
+sudo apt-get update;
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get update;
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y aspnetcore-runtime-3.1
 
 
 # Docker
