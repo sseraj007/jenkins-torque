053b807ed0f4737526e12cecae06c2530b5e1a10
sseraj007 <sseraj007@gmail.com>
2021-09-13 19:28:04 +0530

    Update init.sh

--- applications/jenkins/init.sh
+++ applications/jenkins/init.sh
@@ -12,13 +12,13 @@ echo "==> Starting deployment"
 # Installing .net SDK and Runtime
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb rm packages-microsoft-prod.deb
-sudo apt-get update;
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
 sudo apt-get install -y dotnet-sdk-3.1
 sudo apt-get update;
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
 sudo apt-get install -y aspnetcore-runtime-3.1
 
 
