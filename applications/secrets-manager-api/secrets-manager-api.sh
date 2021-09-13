157d52d2f834a94c2786bc4f4a4a642596efa1b7
sseraj007 <sseraj007@gmail.com>
2021-09-13 13:54:33 +0530

    Update secrets-manager-api.sh

--- applications/secrets-manager-api/secrets-manager-api.sh
+++ applications/secrets-manager-api/secrets-manager-api.sh
@@ -10,9 +10,9 @@ sudo apt install curl -y
 
 echo '==> Extract api artifact to /var/secrets-manager-api'
 mkdir $ARTIFACTS_PATH/drop
-tar -xvf $ARTIFACTS_PATH/api.*.tar.gz -C $ARTIFACTS_PATH/drop/
+tar -xvf $ARTIFACTS_PATH/api.tar.gz -C $ARTIFACTS_PATH/drop/
 mkdir /var/secrets-manager-api/
-tar -xvf $ARTIFACTS_PATH/drop/drop/api.*.tar.gz -C /var/secrets-manager-api
+tar -xvf $ARTIFACTS_PATH/drop/drop/api.tar.gz -C /var/secrets-manager-api
 
 echo 'RELEASE_NUMBER='$RELEASE_NUMBER >> /etc/environment
 echo 'API_BUILD_NUMBER='$API_BUILD_NUMBER >> /etc/environment
