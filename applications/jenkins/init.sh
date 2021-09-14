aac5744dcf902ca70742fa2c3b2c030ea7baf12d
sseraj007 <sseraj007@gmail.com>
2021-09-14 10:50:04 +0530

    Update init.sh

--- applications/jenkins/init.sh
+++ applications/jenkins/init.sh
@@ -21,6 +21,8 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y aspnetcore-runtime-3.1
 
+export PATH="$PATH:$HOME/.dotnet"
+
 
 echo "==> Starting deployment"
 
